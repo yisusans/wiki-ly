0ae0c9c8fbbe363cdca3c59936e8168e35eea995
Ena Bek <enabek@gmail.com>
2016-06-15 15:45:03 -0400

    Add route changes

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,14 @@
 Rails.application.routes.draw do
   get 'welcome/index'
 
-  get 'welcome/index'
-
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
+
+  get 'login' => 'sessions#new'
+  post 'login' => 'sessions#create'
+  delete 'logout' => 'sessions#destroy'
+
+  resources :users, :except => [:index]
 
   # You can have the root of your site routed with "root"
   root 'welcome#index'

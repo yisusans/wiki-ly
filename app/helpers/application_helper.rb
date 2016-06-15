db918c5b0e1ef850f8157b4b468a936d8b507526
Susan Yi <email": null>
2016-06-15 18:04:39 -0400

    add validations for group

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,10 +1,11 @@
 module ApplicationHelper
 
-def redirect_to_login
-  redirect to '/sessions/login' unless logged_in?
-end
+  def redirect_to_login
+    redirect to '/sessions/login' unless logged_in?
+  end
 
-def redirect_unless_admin
-  redirect_to '/' unless current_user.id == @user.id
-end
+  def redirect_unless_admin
+    redirect_to '/' unless current_user.id == @user.id
+  end
+
 end
--- app/models/group.rb
+++ app/models/group.rb
@@ -4,4 +4,6 @@ class Group < ActiveRecord::Base
   has_many :articles
   has_many :users, through: :memberships
 
+  validates :name, presence: true
+
 end

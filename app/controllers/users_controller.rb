8786ca7872aa36dfbb99ed4aae209df9654cba3d
Ena Bek <enabek@gmail.com>
2016-06-15 15:33:28 -0400

    Edit user authentication

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,25 @@
+class UsersController < ApplicationController
+  def new
+    @user = User.new
+  end
+
+  def show
+    @user = User.find_by(id: params[:id])
+  end
+
+  def create
+    @user = User.new(user_params)
+    if @user.save
+      session[:id] = @user.id
+      redirect_to @user
+    else
+      @errors = @user.errors.full_messages
+      render 'new'
+    end
+  end
+
+private
+  def user_params
+    params.require(:user).permit(:email, :password, :username)
+  end
+end

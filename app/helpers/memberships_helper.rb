c519f944fd845b0b8c4f2d3eb36aa5d7fc3a7a5d
Susan Yi <email": null>
2016-06-16 11:09:40 -0400

    user can only join a group once

--- app/helpers/memberships_helper.rb
+++ app/helpers/memberships_helper.rb
@@ -1,2 +1,16 @@
 module MembershipsHelper
+include ApplicationHelper
+
+
+private
+
+  def member_already?
+    memberships.where(user_id: current_user.id).empty?
+  end
+
+  def memberships
+    group_memberships = @group.memberships
+  end
+
+
 end

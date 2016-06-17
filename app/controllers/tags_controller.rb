4a48f2de06b79b8fd249e1b5b72f0d23254bc27a
Chris Praktish <chris.praktish@gmail.com>
2016-06-17 11:15:02 -0400

    Add search for tag show page; add links from article show to tag show; change size of tag area

--- app/controllers/tags_controller.rb
+++ app/controllers/tags_controller.rb
@@ -1,5 +1,19 @@
 class TagsController < ApplicationController
 
+  def show
+    @tag = Tag.find_by(id: params[:id])
+
+    if params[:search]
+      search = params[:search].downcase
+      capitalize_search = search.capitalize
+      @articles = @tag.articles.search(capitalize_search).order("created_at DESC")
+    else
+      @articles = @tag.articles.order('created_at DESC')
+    end
+
+    render '/tags/show'
+  end
+
   def create
     @tag = Tag.find_or_create_by(label: params[:label])
     @article = Article.find_by(id: params[:article_id])
@@ -11,12 +25,5 @@ class TagsController < ApplicationController
       render 'tags/new/'
     end
   end
-
-
-  def show
-    @tag = Tag.find_by(id: params[:id])
-    @articles = Article.where(tag_id: params[:id])
-  end
-
 
 end

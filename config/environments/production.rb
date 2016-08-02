3f05eb58f406f63a55032dce369e8723e5529e3f
Susan Yi <yi.susans@gmail.com>
2016-08-01 21:23:42 -0400

    try to leverage browser cache-ing

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -19,7 +19,8 @@ Rails.application.configure do
   # For large-scale production use, consider using a caching reverse proxy like
   # NGINX, varnish or squid.
   # config.action_dispatch.rack_cache = true
-
+  config.serve_static_assets = true
+  config.static_cache_control = "public, max-age=2419200"
   # Disable serving static files from the `/public` folder by default since
   # Apache or NGINX already handles this.
   config.serve_static_files = ENV['RAILS_SERVE_STATIC_FILES'].present?
